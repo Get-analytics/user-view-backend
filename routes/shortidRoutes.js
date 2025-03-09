c36a7bc920d76a193ca5cf08b99b72ac6ee5f362
Get-analytics <team20tech@gmail.com>
2025-03-09 15:42:25 +0530

    Update shortidRoutes.js
    user identity route addedd

--- routes/shortidRoutes.js
+++ routes/shortidRoutes.js
@@ -1,11 +1,11 @@
 const express = require("express");
 const router = express.Router();
-const { getDocumentByShortId, getAnalyticsPdf, NewUserCount, ReturnedUserCount , webViewAnalytics, Docxanalytics , Videoanalytics} = require("../controllers/shortidController");
+const { getDocumentByShortId, getAnalyticsPdf,  ReturnedUserCount , webViewAnalytics, Docxanalytics , Videoanalytics , userIdentification} = require("../controllers/shortidController");
 
 
 router.get("/viewer/:id", getDocumentByShortId);
 router.post("/pdfpageinfo", getAnalyticsPdf);
-router.post("/newUser", NewUserCount);
+router.post("/user/identify", userIdentification);
 router.post("/existUser", ReturnedUserCount);
 router.post("/webpageinteraction/analytics", webViewAnalytics);
 router.post("/Docx/DocxAnalytics", Docxanalytics);
