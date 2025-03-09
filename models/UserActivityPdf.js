a5becfa41cf3e10b476bb0d35d29ebb42ea38657
Get-analytics <team20tech@gmail.com>
2025-03-09 20:04:44 +0530

    Update UserActivityPdf.js
    most visited page deleted

--- models/UserActivityPdf.js
+++ models/UserActivityPdf.js
@@ -31,7 +31,6 @@ const analyticsSchema = new mongoose.Schema(
     totalClicks: { type: Number, default: 0 },
     inTime: { type: Date, required: true },
     outTime: { type: Date, required: true },
-    mostVisitedPage: { type: String },
     linkClicks: { type: [linkClickSchema], default: [] }
   },
   { timestamps: true } // Automatically adds createdAt and updatedAt fields
