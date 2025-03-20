ee7061ad01c2d6a6a776017a59d3e90b57d0de45
Get-analytics <team20tech@gmail.com>
2025-03-20 20:08:29 +0530

    Update app.js
    new cors origin supported https://view.sendnow.live

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 app.use((req, res, next) => {
-  const allowedOrigins = ['https://filescence-rho.vercel.app', 'http://localhost:3000',  'http://localhost:3001'];
+  const allowedOrigins = ['https://filescence-rho.vercel.app', 'http://localhost:3000',  'http://localhost:3001', 'https://view.sendnow.live'];
   const origin = req.headers.origin;
   if (allowedOrigins.includes(origin)) {
       res.header('Access-Control-Allow-Origin', origin);
